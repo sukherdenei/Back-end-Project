d8273dd81d99c2633460aa352d66391f0a29b140
sukherdenei <sukherdene.ba@gmail.com>
2025-02-10 23:20:40 +0800

    day1

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+// import {express} from `express` require = import
 const express = require("express");
 
 const app = express();
 const port = 4000;
 
 const cors = require("cors");
-
 app.use(cors());
 
-const movie = require("./util/movie-data");
+const movie = require("./util/upcoming-movieData");
 
+// route
 app.get("/movie", (req, res) => {
   res.send(movie);
 });
